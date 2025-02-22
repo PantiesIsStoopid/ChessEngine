33aed8835c4cf41132ca6068262472acc00d975d
Nyle <nylewagjiani@gmail.com>
2025-02-22 15:57:36 +0000

    Add move representation structure and macros for chess moves

--- defs.h
+++ defs.h
@@ -109,6 +109,14 @@ enum
 typedef struct
 {
   int move;
+  int score;
+
+} S_MOVE;
+
+
+typedef struct
+{
+  int move;
   int castlePerm;
   int enPas;
   int fiftyMove;
@@ -145,6 +153,30 @@ typedef struct
   int pList[13][10]; // piece list
 
 } S_BOARD;
+
+/* GAME MOVE */
+
+/*
+0000 0000 0000 0000 0000 0111 1111 -> From 0x7F
+0000 0000 0000 0011 1111 1000 0000 -> To >> 7, 0x7F
+0000 0000 0011 1100 0000 0000 0000 -> Captured >> 14, 0xF
+0000 0000 0100 0000 0000 0000 0000 -> EP 0x40000
+0000 0000 1000 0000 0000 0000 0000 -> Pawn Start 0x80000
+0000 1111 0000 0000 0000 0000 0000 -> Promoted Piece >> 20, 0xF
+0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
+*/
+
+#define FROMSQ(m) ((m) & 0x7F)
+#define TOSQ(m) (((m)>>7) & 0x7F)
+#define CAPTURED(m) (((m)>>14) & 0xF)
+#define PROMOTED(m) (((m)>>20) & 0xF)
+
+#define MFLAGEP 0x40000
+#define MFLAGPS 0x80000
+#define MFLAGCA 0x1000000
+
+#define MFLAGCAP 0x7C000
+#define MFLAGPROM 0xF00000
 
 // MACROS
 #define FR2SQ(f, r) ((21 + (f)) + ((r) * 10))
--- vice.c
+++ vice.c
@@ -2,9 +2,6 @@
 #include "stdlib.h"
 #include "defs.h"
 
-#define FEN1 "rnbqkbnr/pppppppp/8/8/4p3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-#define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4p3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-#define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4p3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 #define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2NQ1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
 int main()
@@ -17,8 +14,16 @@ int main()
   ParseFen(FEN4, board);
 
   PrintBoard(board);
- 
+
   ASSERT(CheckBoard(board));
 
+  int move = 0;
+  int from = 6;
+  int to = 12;
+  int cap = wR;
+  int prom = bR;
+  move = ((from) | (to << 7) | (cap << 14) | (prom << 20));
+  
+  printf("Move: %d\n", move);
   return 0;
 }
